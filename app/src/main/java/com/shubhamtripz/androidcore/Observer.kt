8fbacdfdb5feede265d03f680671b11e6f398000
backpackerdeveloper <contactshubhamtripz@gmail.com>
2024-09-16 15:51:34 +0530

    LifeCycle aware added

---
+++ app/src/main/java/com/shubhamtripz/androidcore/Observer.kt
@@ -0,0 +1,14 @@
+package com.shubhamtripz.androidcore
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class Observer: LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate(){
+        Log.d("MAIN", "Observer - onCreate: ")
+    }
+}
